abdcde38606f088fa888e9d35b42870b449f1728
artSao <arthursaocin@gmail.com>
2024-01-23 23:11:36 +0700

    main: membuat responsive search form

--- Program/JS/script.js
+++ Program/JS/script.js
@@ -1,9 +1,18 @@
-// toogle class active
-
+// toogle class active untuk hamburger menu
 const navbarNav = document.querySelector(".navbar-nav");
 // ketika hamburger menu di klik
 document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
+};
+
+// toogle class active untuk search form
+const searchForm = document.querySelector('.search-form');
+const searchBox =  document.querySelector('#search-box');
+
+document.querySelector('#search-button').onclick = (e) => {
+  searchForm.classList.toggle('active')
+  searchBox.focus();
+  e.preventDefault();
 };
 
 //klik di luar side bar untuk menghilangkan nav
