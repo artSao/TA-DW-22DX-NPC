ddbaee9675b2f1ccd1a1a839b3485939545b327b
artSao <arthursaocin@gmail.com>
2024-01-23 23:38:27 +0700

    main: membuat tambahan informasi

--- Program/JS/script.js
+++ Program/JS/script.js
@@ -15,12 +15,16 @@ document.querySelector('#search-button').onclick = (e) => {
   e.preventDefault();
 };
 
-//klik di luar side bar untuk menghilangkan nav
+//klik di luar element
 
-const hamburger = document.querySelector("#hamburger-menu");
+const hm = document.querySelector("#hamburger-menu");
+const sb = document.querySelector('#search-button')
 
 document.addEventListener("click", function (e) {
-  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
+  }
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
   }
 });
